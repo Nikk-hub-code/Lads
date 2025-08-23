ec64b849e768bd516767effe2ba0021751f319b7
Kaushal Jha <jhakaushal.1809@gmail.com>
2025-08-23 10:55:09 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,12 @@
-user_input = '' # Important Aspect for future
 def lads():
     print("Hey!!I'm LADS.\nYou can ask me things related to Kaushal.\nEnter 'info' to view all the commands")
+    user_input = '' # Important Aspect for future
     exitCommands = ['exit']
-    while user_input != any(exitCommands):          #This logic will atleast run a loop once which will consume resources
+    while user_input not in exitCommands:          #This logic will atleast run a loop once which will consume resources
         user_input = input("> ").lower()
     # while(True):                                  #This logic is the solution for the above issue
     #     user_input = input("> ").lower()
-    #     if(user_input != any(exitCommands)):
+    #     if(user_input != not in exitCommands):
     #         print("EXIT COMMAND ENTERED!!")
     #         break
 
